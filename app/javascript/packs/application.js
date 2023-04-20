042038e652ef7899702c49feefd3f7b1cf0b8e97
touchselect <kir191474@gmail.com>
2023-04-20 09:40:44 +0000

    chapter24

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,16 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import "jquery";
+import "popper.js";
+import "bootstrap";
+import "../stylesheets/application"
+import '@fortawesome/fontawesome-free/js/all'
+
+//= require jquery3
+//= require popper
+//= require bootstrap
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
